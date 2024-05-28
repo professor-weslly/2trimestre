16c5f3306bd5f7411a14664db4a63e46b4421a12
gabrielbazuca <129764422+gabrielbazuca@users.noreply.github.com>
2024-05-28 10:01:17 -0300

    .

--- script.js
+++ script.js
@@ -8,36 +8,36 @@ const perguntas = [
     {
         enunciado:"pergunta 1",
         alternativas:[
-            "Alternativa 1"
-            "Alternativa 2"
+            "Alternativa 1",
+            "Alternativa 2",
                 ]
-    }
+    },
     {
         enunciado:"pergunta 2",
         alternativas:[
-            "Alternativa 1"
-            "Alternativa 2"
+            "Alternativa 1",
+            "Alternativa 2",
                 ]
-    }
+    },
     {
         enunciado:"pergunta 3",
         alternativas:[
-            "Alternativa 1"
-            "Alternativa 2"
+            "Alternativa 1",
+            "Alternativa 2",
                 ]
-    }
+    },
     {
         enunciado:"pergunta 4",
         alternativas:[
-            "Alternativa 1"
-            "Alternativa 2"
+            "Alternativa 1",
+            "Alternativa 2",
                 ]
-    }
+    },
     {
         enunciado:"pergunta 5",
         alternativas:[
-            "Alternativa 1"
-            "Alternativa 2"
+            "Alternativa 1",
+            "Alternativa 2",
                 ]
-    }
+    },  
 ];
